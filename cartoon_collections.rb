95a196d263e66ef9089bacc5db263ca3a98a45c3
Matthew Bartl <mattbartl@gmail.com>
2019-03-20 15:31:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,8 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls_long)
-  # Your code here
+  calls_long.any? |word|
+  
 end
 
 def find_the_cheese# code an argument here
